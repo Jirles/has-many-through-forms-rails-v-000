e4ac7465c72d49adb73222d365a8104404186117
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 15:25:02 +0000

    automatically backed up by learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
    before do
-     @post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
-     @post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
+     post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
+     post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
      @post_3 = Post.create(title: "Guess what?!", content: "I wrote this spec!")
 
      @cat_1 = Category.create(name: "Exciting")
-     @cat_2 = Category.create(name: "Meh")
+     cat_2 = Category.create(name: "Meh")
 
      PostCategory.create(post: post_1, category: cat_1)
      PostCategory.create(post: post_2, category: cat_2)
