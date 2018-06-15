72727d551b6fc27ab9b18cf920504d49aa35153e
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 15:23:52 +0000

    automatically backed up by learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Category, type: :model do
    before do
      post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
      post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
-     post_3 = Post.create(title: "Guess what?!", content: "I wrote this spec!")
+     @post_3 = Post.create(title: "Guess what?!", content: "I wrote this spec!")
 
-     cat_1 = Category.create(name: "Exciting")
+     @cat_1 = Category.create(name: "Exciting")
      cat_2 = Category.create(name: "Meh")
 
      PostCategory.create(post: post_1, category: cat_1)
@@ -14,11 +14,11 @@ RSpec.describe Category, type: :model do
      PostCategory.create(post: post_3, category: cat_1)
    end
   it "has a name" do
-    expect(cat_1.name).to eq("Exciting")
+    expect(@cat_1.name).to eq("Exciting")
   end
 
   it 'has many posts through post_categories' do
-    expect(cat_1.posts).to include(post_3)
-    expect(cat_1.posts.count).to eq(2)
+    expect(@cat_1.posts).to include(@post_3)
+    expect(@cat_1.posts.count).to eq(2)
   end
 end
