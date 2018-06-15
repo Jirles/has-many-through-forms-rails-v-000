241c14ae46f97e48aa6856d70628dba01d8f0078
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 15:13:37 +0000

    automatically backed up by learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   before do
+     post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
+     post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
+   end
+  it ""
 end
